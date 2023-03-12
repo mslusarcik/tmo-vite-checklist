7858b26b9149a19882fa55ed56f574a7b088362a
mslusarcik <miroslav@slusarcik.cz>
2023-03-12 12:11:32 +0100

    Feature tu update old data saved in localStorage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,12 +5,32 @@ import Card from '../components/Card';
 import ChecklistItem from '../components/ChecklistItem';
 import ResetCompleted from '../components/ResetCompleted';
 import logo from '../assets/logo.svg';
+import originData from '../data';
 
 const index = () => {
-  const { data } = useContext(dataContext);
+  const { data, setData } = useContext(dataContext);
+
+  const handleDataRefresh = (e) => {
+    e.preventDefault();
+
+    console.log('Trying to reset data');
+    setData(originData);
+  };
+
   return (
     <>
       <Card size='md'>
+        <p className='text-red-400 text-xs text-right'>
+          V případě, že máte zastaralou verzi,{' '}
+          <span
+            className='cursor-pointer underline'
+            onClick={(e) => {
+              handleDataRefresh(e);
+            }}>
+            aktualizujte data
+          </span>
+          .
+        </p>
         <h1 className='mt-0 mb-2 pb-2 flex items-center'>
           <img
             src={logo}
