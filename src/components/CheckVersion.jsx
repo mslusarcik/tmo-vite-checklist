ae5f22ab17ccec1e838d3b23936fdf5e5d0bcdb1
mslusarcik <miroslav@slusarcik.cz>
2023-03-12 13:13:27 +0100

    Auto version checker for data updates

---
+++ src/components/CheckVersion.jsx
@@ -0,0 +1,45 @@
+import React, { useState, useContext, useEffect } from 'react';
+import { dataContext } from '../context/dataContext';
+import useLocalStorage from '../hooks/useLocalStorage';
+import originData from '../data';
+
+const CheckVersion = () => {
+  const [storageVersion, setStorageVersion] = useLocalStorage('msVersion', '');
+  const { setData } = useContext(dataContext);
+  const currentVersion = document.body.dataset.version;
+
+  if (!storageVersion) {
+    setStorageVersion(currentVersion);
+  }
+
+  const handleDataRefresh = (e) => {
+    e.preventDefault();
+    setStorageVersion(currentVersion);
+  };
+
+  useEffect(() => {
+    setData(originData);
+  }, [storageVersion]);
+
+  return (
+    <>
+      {storageVersion !== currentVersion ? (
+        <p className='text-red-400 text-xs text-right'>
+          Vypadá to, že máte zastaralou verzi. Prosím{' '}
+          <span
+            className='cursor-pointer underline'
+            onClick={(e) => {
+              handleDataRefresh(e);
+            }}>
+            aktualizujte data
+          </span>
+          .
+        </p>
+      ) : (
+        <p className='text-green-400 text-xs text-right'>Máte aktuální verzi</p>
+      )}
+    </>
+  );
+};
+
+export default CheckVersion;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,33 +4,16 @@ import { dataContext } from '../context/dataContext';
 import Card from '../components/Card';
 import ChecklistItem from '../components/ChecklistItem';
 import ResetCompleted from '../components/ResetCompleted';
+import CheckVersion from '../components/CheckVersion';
 import logo from '../assets/logo.svg';
-import originData from '../data';
 
 const index = () => {
-  const { data, setData } = useContext(dataContext);
-
-  const handleDataRefresh = (e) => {
-    e.preventDefault();
-
-    console.log('Trying to reset data');
-    setData(originData);
-  };
+  const { data } = useContext(dataContext);
 
   return (
     <>
       <Card size='md'>
-        <p className='text-red-400 text-xs text-right'>
-          V případě, že máte zastaralou verzi,{' '}
-          <span
-            className='cursor-pointer underline'
-            onClick={(e) => {
-              handleDataRefresh(e);
-            }}>
-            aktualizujte data
-          </span>
-          .
-        </p>
+        <CheckVersion />
         <h1 className='mt-0 mb-2 pb-2 flex items-center'>
           <img
             src={logo}
