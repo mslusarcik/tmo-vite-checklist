bbc3e0eec237d6d8888c008804b749361f24e347
mslusarcik <miroslav@slusarcik.cz>
2023-03-14 01:29:16 +0100

    base removed from url

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   assetsInclude: ['**/*.md'],
-  base: '/tmo-vite-checklist/',
+  // base: '/tmo-vite-checklist/',
 });
